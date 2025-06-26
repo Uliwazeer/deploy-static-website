9045a58c4811f8f611f30030a893b61335805418
Ali Wazeer <84068430+Uliwazeer@users.noreply.github.com>
2025-06-26 22:53:44 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
+
 output "cloudfront_url" {
   description = "The CloudFront distribution URL"
   value       = "https://${aws_cloudfront_distribution.cdn.domain_name}"
 }
-=======
-output "cloudfront_url" {
-  description = "The CloudFront distribution URL"
-  value       = "https://${aws_cloudfront_distribution.cdn.domain_name}"
-}
->>>>>>> 257e586 (deploy)
+
