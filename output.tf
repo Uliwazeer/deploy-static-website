277726ffc94ac741a51f2bc67e4439419c4f7f85
Uliwazeer <ali.wazeer2000@gmail.com>
2025-06-26 20:30:41 +0300

    Deploy

---
+++ output.tf
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+output "cloudfront_url" {
+  description = "The CloudFront distribution URL"
+  value       = "https://${aws_cloudfront_distribution.cdn.domain_name}"
+}
+=======
+output "cloudfront_url" {
+  description = "The CloudFront distribution URL"
+  value       = "https://${aws_cloudfront_distribution.cdn.domain_name}"
+}
+>>>>>>> 257e586 (deploy)
---
+++ variables.tf
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "deploy-static-site47450"
+}
+=======
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "deploy-static-site47450"
+}
+>>>>>>> 257e586 (deploy)
